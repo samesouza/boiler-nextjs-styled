5e4404c2dc4e14d87c1a5fa2c9ad266856605bf4
Samuel Souza <samuel.oliveira.souza@hotmail.com>
2024-02-27 10:40:41 +0000

    feat: config typescript on styled-components

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,9 @@ import withPWAInit from 'next-pwa'
 
 const isProd = process.env.NODE_ENV === 'production'
 const withPWA = withPWAInit({
+  compiler: {
+    styledComponents: true,
+  },
   dest: 'public',
   disable: !isProd,
 })
